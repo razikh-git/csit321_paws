35f3c47fa5dc45e8997337776e4b4b937ef881f8
csit321_ktw02a <pocketsathome@gmail.com>
2019-09-20 19:37:54 +1000

    ctrl+s

--- app/src/main/java/com/example/csit321_paws/HomeActivity.java
+++ app/src/main/java/com/example/csit321_paws/HomeActivity.java
@@ -1,31 +1,127 @@
 package com.example.csit321_paws;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.Manifest;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.format.DateFormat;
+import android.util.Log;
+import android.widget.TextView;
 
-public class HomeActivity extends AppCompatActivity {
+import java.util.Collections;
+import java.util.HashMap;
+
+public class HomeActivity extends PermissionActivity {
+
+    private static final String[] REQUEST_PERMISSIONS_LOCATION = {
+            Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION};
+
+    private SharedPreferences mSharedPref;
+    private SharedPreferences.Editor mSharedEditor;
+    private PAWSLocationHandler mLocHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        SharedPreferences sharedPref = this.getPreferences(Context.MODE_PRIVATE);
-        SharedPreferences.Editor sharedEditor = sharedPref.edit();
+        mSharedPref = this.getPreferences(Context.MODE_PRIVATE);
+        mSharedEditor = mSharedPref.edit();
 
-        // TODO : rehouse this call elsewhere
+        initStrings();
+        initLocationData();
+    }
 
-        // Generate URL and request OWM data
+    private boolean initStrings() {
+        try {
+            // Initialise map of request and permission codes.
+            mCodeMap = new HashMap<>();
+            mCodeMap.put(Manifest.permission.ACCESS_COARSE_LOCATION,
+                    RequestCode.PERMISSION_LOCATION_COARSE);
+            mCodeMap.put(Manifest.permission.ACCESS_FINE_LOCATION,
+                    RequestCode.PERMISSION_LOCATION_FINE);
+            mCodeMap = Collections.unmodifiableMap(mCodeMap);
 
-        // Determine warnings and layout.
+            // Initialise map of title messages.
+            mTitleMap = new HashMap<>();
+            mTitleMap.put(Manifest.permission.ACCESS_COARSE_LOCATION,
+                    getResources().getString(R.string.app_title_request_loc_coarse));
+            mTitleMap.put(Manifest.permission.ACCESS_FINE_LOCATION,
+                    getResources().getString(R.string.app_title_request_loc_fine));
 
-        // Initialise location information.
+            // Initialise map of request messages.
+            mMessageMap = new HashMap<>();
+            mMessageMap.put(Manifest.permission.ACCESS_COARSE_LOCATION,
+                    getResources().getString(R.string.app_msg_request_loc_coarse));
+            mMessageMap.put(Manifest.permission.ACCESS_FINE_LOCATION,
+                    getResources().getString(R.string.app_msg_request_loc_fine));
 
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
 
-        // Initialise weather conditions.
+    // TODO : store last-known location in binary, allow for out-of-service use of old data
+
+    private boolean initLocationData() {
+        PAWSLocation loc = null;
+        mLocHandler = new PAWSLocationHandler(getApplicationContext());
+
+        Log.println(Log.DEBUG, "snowpaws", "welcome to initLocationData");
+
+        if (checkHasPermissions(RequestCode.PERMISSION_MULTIPLE, REQUEST_PERMISSIONS_LOCATION)) {
+
+            Log.println(Log.DEBUG, "snowpaws", "i'm in");
+
+            // Fetch location data.
+            TextView text;
+            loc = mLocHandler.getPAWSLocation();
+            if (loc != null) {
+                // Initialise location displays.
+                text = findViewById(R.id.txtCity);
+                text.setText(loc.name());
+
+                text = findViewById(R.id.txtTimestampTop);
+                text.setText(DateFormat.format("HH:mm:ss", loc.timestamp()).toString());
+
+                text = findViewById(R.id.txtTimestampBottom);
+                text.setText(DateFormat.format("dd-MM-yyyy", loc.timestamp()).toString());
+
+                // Generate URL and request OWM data.
+
+                // Initialise weather conditions.
+
+                return true;
+            }
+
+            // If no latest location could be fetched, post fallback info.
+
+            // . . .
+
+        } else {
+            // Determine warnings and layout.
+
+            // . . .
+
+        }
+
+        return false;
+    }
+
+    @Override
+    protected void onPermissionGranted(String perm) {
+
+    }
+
+    @Override
+    protected void onPermissionBlocked(String perm) {
+
+    }
+
+    @Override
+    protected void onAllPermissionsGranted(String[] permissions) {
 
     }
 }
---
+++ app/src/main/java/com/example/csit321_paws/RequestCode.java
@@ -0,0 +1,8 @@
+package com.example.csit321_paws;
+
+interface RequestCode {
+    int PERMISSION_MULTIPLE = 0;
+    int PERMISSION_LOCATION_FINE = 1;
+    int PERMISSION_LOCATION_COARSE = 2;
+
+}
