4c42bf0ae1f86d102d8d653f8e54351676c08e5f
razikh-git <pocketsathome@gmail.com>
2020-02-20 17:37:02 +1100

    Raise weather activity on touch

--- app/src/main/java/com/amw188/csit321_paws/DailyWeatherWorker.java
+++ app/src/main/java/com/amw188/csit321_paws/DailyWeatherWorker.java
@@ -4,7 +4,9 @@ import android.annotation.TargetApi;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Build;
 import android.util.Log;
@@ -61,8 +63,6 @@ public class DailyWeatherWorker extends Worker {
     }
 
     private Result pushWeatherNotification(Context context) {
-        Log.i(TAG, "in pushWeatherNotification()");
-
         NotificationManager manager = (NotificationManager) context.getSystemService(
                 Context.NOTIFICATION_SERVICE);
         if (manager == null) {
@@ -78,13 +78,15 @@ public class DailyWeatherWorker extends Worker {
     }
 
     private Notification getWeatherNotification(Context context) {
-        Log.i(TAG, "in getWeatherNotification()");
-
+        PendingIntent contentIntent = PendingIntent.getActivity(
+                context, 0, new Intent(context, WeatherActivity.class),
+                PendingIntent.FLAG_UPDATE_CURRENT);
         NotificationCompat.Builder builder = new NotificationCompat.Builder(
                 context, WEATHER_CHANNEL_ID)
                 .setSmallIcon(R.drawable.ic_paws_icon)
-                .setContentText("YOU SAY GOODBYE")
-                .setContentTitle("I DONT KNOW WHY YOU SAY GOODBYE")
+                .setContentIntent(contentIntent)
+                .setContentText("CONTENT TEXT, HELLO HELLO")
+                .setContentTitle("Weather over {AREA}: {0}")
                 .setPriority(Notification.PRIORITY_DEFAULT);
         return builder.build();
     }
